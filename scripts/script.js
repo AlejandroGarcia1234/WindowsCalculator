2cc887d389a6d8cdb35e73bfa4190010a4c43c05
Alejandro Garcia <jandro_es@hotmail.com>
2023-10-11 20:07:21 +0200

    Modificamos el script para solucionar errores

--- scripts/script.js
+++ scripts/script.js
@@ -6,119 +6,269 @@ let operator_value;
 let last_button;
 let calc_operator;
 let total;
-const [box, history, equal, dot] = ["box", "last_operation_history", "equal_sign", "dot"].map(e => document.getElementById(e));
+const box = document.getElementById("box");
+const last_operation_history = document.getElementById("last_operation_history");
+const equal = document.getElementById("equal_sign").value;
+const dot = document.getElementById("dot").value;
 
-const key_combination = {};
+const key_combination = [];
 
-const button_number = button => {
+function button_number(button) {
     last_button = button;
+
     if (!operators.includes(button) && button !== equal) {
-        if (firstNum) box.innerText = (button === dot) ? "0" + dot : button;
-        else if (box.innerText.length > 1 || button !== dot) box.innerText = (box.innerText.length === 1 && box.innerText === "0") ? (button === dot) ? "0" + dot : button : (box.innerText.includes(dot) && button === dot) ? box.innerText : (box.innerText.length < 20) ? (button === dot && box.innerText === "-") ? "-0" + dot : box.innerText + button : box.innerText;
-        firstNum = false;
-    } else {
-        if (operator_value && button === operator_value) return;
-        if (button === "-" && !numbers.length && box.innerText == 0) return (box.innerText = button, firstNum = false, operator_value = button, showSelectedOperator());
-        if (operators.includes(button) && box.innerText === "-") return;
-        if (button === "-" && operator_value === "-" && history.innerText.includes("=")) return;
-        if (operators.includes(button)) calc_operator = (last_operator !== undefined) && last_operator !== null ? last_operator : button;
-        last_operator = button === "*" ? "×" : button === "/" ? "÷" : button;
-        operator_value = button;
-        firstNum = true;
-        showSelectedOperator();
-        if (!numbers.length) {
-            numbers.push(box.innerText);
-            if (last_operator !== undefined && last_operator !== null) history.innerText = box.innerText + " " + last_operator;
+        if (firstNum) {
+            box.innerText = button === dot ? "0" + dot : button;
+            firstNum = false;
         } else {
-            if (numbers.length === 1) numbers[1] = box.innerText;
+            if (box.innerText.length === 1 && box.innerText === "0") {
+                if (button === dot) {
+                    box.innerText += button;
+                }
+                return;
+            }
+            if (box.innerText.includes(dot) && button === dot) {
+                return;
+            }
+            if (box.innerText.length === 20) {
+                return;
+            }
+            if (button === dot && box.innerText === "-") {
+                box.innerText = "-0" + dot;
+            } else {
+                box.innerText += button;
+            }
+        }
+    } else {
+        if (operator_value !== null && button === operator_value) {
+            return;
+        }
+        if (button === "-" && box.innerText == 0) {
+            box.innerText = button;
+            firstNum = false;
+            operator_value = button;
+            showSelectedOperator();
+            return;
+        }
+        if (operators.includes(button) && box.innerText === "-") {
+            return;
+        }
+        if (button === "-" && operator_value === "-" && last_operation_history.innerText.includes("=")) {
+            return;
+        }
+        if (operators.includes(button)) {
+            calc_operator = typeof last_operator !== "undefined" && last_operator !== null ? last_operator : button;
+            last_operator = button === "*" ? "×" : button === "/" ? "÷" : button;
+            operator_value = button;
+            firstNum = true;
+            showSelectedOperator();
+        }
+        if (numbers.length === 0) {
+            numbers.push(box.innerText);
+            if (typeof last_operator !== "undefined" && last_operator !== null) {
+                last_operation_history.innerText = box.innerText + " " + last_operator;
+            }
+        } else {
+            if (numbers.length === 1) {
+                numbers[1] = box.innerText;
+            }
             const temp_num = box.innerText;
             if (button === equal && calc_operator !== null) {
                 total = calculate(numbers[0], numbers[1], calc_operator);
                 box.innerText = total;
-                if (!history.innerText.includes("=")) history.innerText += " " + numbers[1] + " =";
+                if (!last_operation_history.innerText.includes("=")) {
+                    last_operation_history.innerText += " " + numbers[1] + " =";
+                }
                 temp_num = numbers[0];
                 numbers[0] = total;
                 operator_value = null;
                 showSelectedOperator();
-                const history_arr = history.innerText.split(" ");
+                const history_arr = last_operation_history.innerText.split(" ");
                 history_arr[0] = temp_num;
-                history.innerText = history_arr.join(" ");
+                last_operation_history.innerText = history_arr.join(" ");
             } else if (calc_operator !== null) {
-                history.innerText = temp_num + " " + last_operator;
+                last_operation_history.innerText = temp_num + " " + last_operator;
                 calc_operator = button;
                 numbers = [];
                 numbers.push(box.innerText);
             }
         }
     }
-};
+}
 
-const showSelectedOperator = () => {
+function showSelectedOperator() {
     const elements = document.getElementsByClassName("operator");
-    for (const element of elements) element.style.backgroundColor = "#e68a00";
-    const colorMap = { "+": "plusOp", "-": "subOp", "*": "multiOp", "/": "divOp" };
-    document.getElementById(colorMap[operator_value] || "").style.backgroundColor = "#ffd11a";
-};
-
-const calculate = (num1, num2, operator) => {
-    switch (operator) {
-        case "+": return parseFloat(num1) + parseFloat(num2);
-        case "-": return parseFloat(num1) - parseFloat(num2);
-        case "*": return parseFloat(num1) * parseFloat(num2);
-        case "/": return parseFloat(num1) / parseFloat(num2);
-        default: return (total === box.innerText) ? total : box.innerText;
+    for (const element of elements) {
+        element.style.backgroundColor = "#e68a00";
     }
-};
+    if (operator_value === "+") {
+        document.getElementById("plusOp").style.backgroundColor = "#ffd11a";
+    } else if (operator_value === "-") {
+        document.getElementById("subOp").style.backgroundColor = "#ffd11a";
+    } else if (operator_value === "*") {
+        document.getElementById("multiOp").style.backgroundColor = "#ffd11a";
+    } else if (operator_value === "/") {
+        document.getElementById("divOp").style.backgroundColor = "#ffd11a";
+    }
+}
 
-const button_clear = () => window.location.reload();
+function calculate(num1, num2, operator) {
+    if (operator === "+") {
+        total = parseFloat(num1) + parseFloat(num2);
+    } else if (operator === "-") {
+        total = parseFloat(num1) - parseFloat(num2);
+    } else if (operator === "*") {
+        total = parseFloat(num1) * parseFloat(num2);
+    } else if (operator === "/") {
+        total = parseFloat(num1) / parseFloat(num2);
+    } else {
+        if (total === box.innerText) {
+            return total;
+        } else {
+            return box.innerText;
+        }
+    }
+    if (!Number.isInteger(total)) {
+        total = total.toPrecision(12);
+    }
+    return parseFloat(total);
+}
 
-const backspace_remove = () => {
-    box.innerText = (box.innerText.slice(0, -1) || 0);
-    firstNum = (box.innerText.length === 0);
-};
+function button_clear() {
+    window.location.reload();
+}
 
-const plus_minus = () => {
-    if (operator_value && numbers.length) {
+function backspace_remove() {
+    const last_num = box.innerText.slice(0, -1);
+    box.innerText = last_num;
+    if (box.innerText.length === 0) {
+        box.innerText = 0;
+        firstNum = true;
+    }
+}
+
+function plus_minus() {
+    if (operator_value && numbers.length > 0) {
         if (operators.includes(last_button)) {
             if (box.innerText === "-") {
                 box.innerText = 0;
                 firstNum = true;
-            } else box.innerText = (box.innerText === 0) ? "-" : -box.innerText;
-            if (numbers.length === 1) numbers[0] = box.innerText;
-            else numbers[1] = box.innerText;
+                return;
+            } else {
+                box.innerText = "-";
+                firstNum = false;
+            }
+        } else {
+            box.innerText = -box.innerText;
+            if (numbers.length === 1) {
+                numbers[0] = box.innerText;
+            } else {
+                numbers[1] = box.innerText;
+            }
         }
-    } else box.innerText = (box.innerText === 0) ? "-" : -box.innerText;
-};
+        return;
+    }
+    if (box.innerText === 0) {
+        box.innerText = "-";
+        firstNum = false;
+    } else {
+        box.innerText = -box.innerText;
+    }
+}
 
-const square_root = () => box.innerText = Math.sqrt(box.innerText);
+function square_root() {
+    const square_num = Math.sqrt(box.innerText);
+    box.innerText = square_num;
+    numbers.push(square_num);
+}
 
-const division_one = () => box.innerText = 1 / box.innerText;
+function division_one() {
+    const square_num = 1 / box.innerText;
+    box.innerText = square_num;
+    numbers.push(square_num);
+}
 
-const power_of = () => box.innerText = Math.pow(box.innerText, 2);
+function power_of() {
+    const square_num = Math.pow(box.innerText, 2);
+    box.innerText = square_num;
+    numbers.push(square_num);
+}
 
-const calculate_percentage = () => {
-    if (numbers.length && last_operator) box.innerText = (last_operator === "+" || last_operator === "-") ? (numbers * box.innerText) / 100 : (box.innerText / 100);
-    else box.innerText /= 100;
-    numbers = [box.innerText];
-    for (const element of document.getElementsByClassName("operator")) element.style.backgroundColor = "#e68a00";
-};
+function calculate_percentage() {
+    const elements = document.getElementsByClassName("operator");
+    if (numbers.length > 0 && last_operator) {
+        if (last_operator === "+" || last_operator === "-") {
+            box.innerText = (numbers * box.innerText) / 100;
+        } else {
+            box.innerText = box.innerText / 100;
+        }
+    } else {
+        box.innerText = box.innerText / 100;
+    }
+    numbers = [];
+    numbers.push(box.innerText);
+    for (const element of elements) {
+        element.style.backgroundColor = "#e68a00";
+    }
+}
 
-const clear_entry = () => (numbers.length && last_operator) ? (box.innerText = 0, numbers = [numbers[0]], firstNum = true) : undefined;
+function clear_entry() {
+    if (numbers.length > 0 && last_operator) {
+        box.innerText = 0;
+        const temp = numbers[0];
+        numbers = [];
+        numbers.push(temp);
+        firstNum = true;
+    }
+}
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', keyPressed);
+document.addEventListener('keyup', keyReleased);
+
+function keyPressed(e) {
     e.preventDefault();
-    const button = e.key === "Enter" ? equal : e.key === "." ? dot : isFinite(e.key) ? e.key : undefined;
-    if (button) button_number(button);
-    if (e.key === "Backspace") {
-        document.getElementById("backspace_btn").style.backgroundColor = "#999999";
-        backspace_remove();
+    if (e.key === "Delete") {
+        button_clear();
+        return;
+    }
+    const isNumber = isFinite(e.key);
+    const enterPress = e.key === "Enter" ? equal : undefined;
+    const dotPress = e.key === "." ? dot : undefined;
+    const commaPress = e.key === "," ? true : false;
+    if (isNumber || operators.includes(e.key) || e.key === enterPress || e.key === dotPress || commaPress || e.key === "Backspace") {
+        if (e.key === "Enter") {
+            button_number(enterPress);
+        } else if (e.key === "Backspace") {
+            document.getElementById("backspace_btn").style.backgroundColor = "#999999";
+            backspace_remove();
+        } else if (commaPress) {
+            button_number(dot);
+        } else {
+            button_number(e.key);
+        }
     }
     key_combination[e.code] = e.key;
-});
+}
 
-document.addEventListener('keyup', e => {
-    if (key_combination['ControlLeft'] === 'v') navigator.clipboard.readText().then(text => (isFinite(text)) && button_number(text)).catch(err => console.error('Failed to read clipboard contents: ', err));
-    if (key_combination['ControlLeft'] === 'c') navigator.clipboard.writeText(box.innerText);
-    delete key_combination[e.code];
-    if (e.key === "Backspace") document.getElementById("backspace_btn").style.backgroundColor = "#666666";
-});
+function keyReleased(e) {
+    if (key_combination['ControlLeft'] && key_combination['KeyV']) {
+        navigator.clipboard.readText().then(text => {
+            if (isFinite(text)) {
+                const copy_number = text;
+                firstNum = true;
+                button_number(copy_number);
+            }
+        }).catch(err => {
+            console.error('Failed to read clipboard contents: ', err);
+        });
+    }
+    if (key_combination['ControlLeft'] && key_combination['KeyC']) {
+        navigator.clipboard.writeText(box.innerText);
+    }
+    key_combination = [];
+    e.preventDefault();
+    if (e.key === "Backspace") {
+        document.getElementById("backspace_btn").style.backgroundColor = "#666666";
+    }
+}
+
